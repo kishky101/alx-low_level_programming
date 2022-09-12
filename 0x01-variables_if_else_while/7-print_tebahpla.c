9a44cf66834c1f01265b19bc7c0be776460b9a6c
kishky <kishky.101@gmail.com>
2022-09-11 17:32:21 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 26; i > 0; i--)
+{
+putchar(alpha[i]);
+}
+putchar('\n');
+return (0);
+}
